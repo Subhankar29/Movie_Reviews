3f3e01fd3dd1ec5b23fafc187418d0a4c2d6cbb3
subhankar29 <subhankar29shah@gmail.com>
2017-02-08 11:10:45 +0530

    Added Toy Story movie trailer and review

---
+++ entertainment.py
@@ -0,0 +1,7 @@
+import media
+
+toy_story = media.Movie("Toy Story",
+                        "A little boy named Andy loves to be in his room, playing with his toys, especially his doll named. But, what do the toys do when Andy is not with them, they come to life. Woody believes that he has life (as a toy) good. However, he must worry about Andy's family moving, and what Woody does not know is about Andy's birthday party. Woody does not realize that Andy's mother gave him an action figure known as Buzz Lightyear, who does not believe that he is a toy, and quickly becomes Andy's new favorite toy. Woody, who is now consumed with jealousy, tries to get rid of Buzz. Then, both Woody and Buzz are now lost. They must find a way to get back to Andy before he moves without them, but they will have to pass through a ruthless toy killer, Sid Phillips.",
+                        "http://www.impawards.com/1995/posters/toy_story_ver1.jpg",
+                        "https://youtu.be/KYz2wyBy3kc")
+print(toy_story.storyline)
